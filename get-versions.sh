19298bc3e832cf92ae428fb9915b2c118ebde87a
Alexander Stepchenko <geochip@altlinux.org>
2024-08-22 11:36:44 +0300

    update script

--- get-versions.sh
+++ get-versions.sh
@@ -4,11 +4,24 @@ TAGS="$(git tag --sort=version:refname | grep -E "^v1\.[2-3][0-9]\.[0-9][0-9]?$"
 
 echo "k8s | coredns | etcd"
 
+previous_coredns_version=
+previous_etcd_version=
 for tag in $TAGS; do
 	git switch -q --detach "$tag"
 	coredns_version="$(grep "CoreDNSVersion =" cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"v(.*)".*/\1/')"
 	etcd_version="$(grep "DefaultEtcdVersion =" cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"(.*)".*/\1/')"
-	echo "$tag | $coredns_version | $etcd_version"
+
+	printf "$tag | $coredns_version | $etcd_version"
+	if [ "$coredns_version" != "$previous_coredns_version" ]; then
+		printf " *coredns*"
+	fi
+	if [ "$etcd_version" != "$previous_etcd_version" ]; then
+		printf " *etcd*"
+	fi
+	printf "\n"
+
+	previous_coredns_version="$coredns_version"
+	previous_etcd_version="$etcd_version"
 done
 
 git switch -q get-versions
