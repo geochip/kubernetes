a19dd5ed009f5b6371545c003165b2bbe32a9db7
Alexander Stepchenko <geochip@altlinux.org>
2025-02-20 00:29:23 +0300

    Update get-versions.sh

--- get-versions.sh
+++ get-versions.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env sh
 
-TAGS="$(git tag --sort=version:refname | grep -E "^v1\.[2-3][0-9]\.[0-9][0-9]?$" | grep -v "v1.2[0-5]")"
+TAGS="$(git tag --sort=version:refname | grep -E '^v1\.[2-3][0-9]\.[0-9]{1,2}$' | grep -v 'v1.2[0-6]')"
 
-echo "k8s | coredns | etcd | pause"
+echo 'k8s | coredns | etcd | pause'
 
 previous_coredns_version=
 previous_etcd_version=
 for tag in $TAGS; do
 	git switch -q --detach "$tag"
-	coredns_version="$(grep "CoreDNSVersion =" cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"v(.*)".*/\1/')"
-	etcd_version="$(grep "DefaultEtcdVersion =" cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"(.*)".*/\1/')"
-	pause_version="$(grep "PauseVersion =" cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"(.*)".*/\1/')"
+	coredns_version="$(grep 'CoreDNSVersion =' cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"v(.*)".*/\1/')"
+	etcd_version="$(grep 'DefaultEtcdVersion =' cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"(.*)".*/\1/')"
+	pause_version="$(grep 'PauseVersion =' cmd/kubeadm/app/constants/constants.go | sed -E 's/.*"(.*)".*/\1/')"
 
-	printf "$tag | $coredns_version | $etcd_version | $pause_version"
+	printf '%s | %s | %s | %s' "$tag" "$coredns_version" "$etcd_version" "$pause_version"
 	if [ "$coredns_version" != "$previous_coredns_version" ]; then
-		printf " *coredns*"
+		printf ' *coredns*'
 	fi
 	if [ "$etcd_version" != "$previous_etcd_version" ]; then
-		printf " *etcd*"
+		printf ' *etcd*'
 	fi
 	if [ "$pause_version" != "$previous_pause_version" ]; then
-		printf " *pause*"
+		printf ' *pause*'
 	fi
-	printf "\n"
+	printf '\n'
 
 	previous_coredns_version="$coredns_version"
 	previous_etcd_version="$etcd_version"
